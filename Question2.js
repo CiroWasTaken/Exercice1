b7ef57c0f636d26a5d44bf8951fe4cc177160a02
Musa Turna <musat@MacBook-de-Musa.local>
2024-05-15 12:45:54 +0200

    terminé

---
+++ Question2.js
@@ -0,0 +1,18 @@
+Question4Test.js
+
+//maskify('4556364607935616') -> '############5616'
+//maskify('1') -> '1'
+//maskify('11111') -> '#1111'
+
+function maskify(cc) {
+    if (cc.length <= 4) {
+        return cc;
+    }
+    var visible = cc.slice(-4);  
+    var masqué = "#".repeat(cc.length - 4);
+    return masqué + visible;
+}
+
+var input = prompt("Veuillez entrer votre numéro de carte :");
+var InputMasqué = maskify(input);
+console.log(InputMasqué);
